93971c9f7265af91714358e231df2de396f9b8c3
Vignesh <vigneshkaruppusamy958@gmail.com>
2025-01-11 19:01:52 +0530

    Create 4bit-multiplier.rtl.v

---
+++ 4bit-multiplier.rtl.v
@@ -0,0 +1,17 @@
+module multiplier_4bit_rtl(
+
+    input [3:0] A,      
+
+    input [3:0] B,       
+
+    output reg [7:0] P);
+
+
+
+always @ (A or B) begin
+
+    P = A * B;           
+
+end
+
+endmodule
